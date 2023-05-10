090aeb93886ae786293bfacb97cfe9703554477c
shiroichiheisen <danielbritoalves2001@outlook.com>
2023-05-10 17:40:08 -0300

    major cleaning of the library

--- ch455.cpp
+++ ch455.cpp
@@ -1,7 +1,7 @@
 #include "ch455.h"
 #include "Wire.h"
 
-void send(int id, int data)
+void send(uint8_t id, uint8_t data)
 {
     Wire.beginTransmission(id);
     Wire.write(data);
@@ -14,33 +14,13 @@ void ch455::brightness(uint8_t brightness)
         brightness = 8;
     else if (brightness < 1)
         brightness = 1;
-    switch (brightness)
-    {
-    case 1:
-        send(36, 0x11);
-        break;
-    case 2:
-        send(36, 0x21);
-        break;
-    case 3:
-        send(36, 0x31);
-        break;
-    case 4:
-        send(36, 0x41);
-        break;
-    case 5:
-        send(36, 0x51);
-        break;
-    case 6:
-        send(36, 0x61);
-        break;
-    case 7:
-        send(36, 0x71);
-        break;
-    case 8:
-        send(36, 0x01);
-        break;
-    }
+
+    uint8_t bright = 1;
+
+    if (brightness < 8)
+        bright += (16 * brightness);
+
+    send(36, bright);
 }
 
 ch455::ch455() {}
@@ -59,94 +39,49 @@ void ch455::begin(uint8_t sda, uint8_t scl, uint8_t brightness)
 
 void ch455::digit(uint8_t digit, uint8_t number, bool dot)
 {
-    if (number > 9)
-        Serial.println("Number is too big, max is 9");
-
-    int digitId;
-
-    switch (digit)
+    if (number > 9 || digit > 3)
     {
-    case 0:
-        digitId = 52;
-        break;
+        Serial.println("CH455 Library: Number or digit is too big, max 9 for number and max 3 for digit");
+        return;
+    }
+
+    uint8_t digitId = 52 + digit;
+
+    uint8_t digitData = 0x3F;
+
+    switch (number)
+    {
     case 1:
-        digitId = 53;
+        digitData = 0x06;
         break;
     case 2:
-        digitId = 54;
+        digitData = 0x5B;
         break;
     case 3:
-        digitId = 55;
+        digitData = 0x4F;
+        break;
+    case 4:
+        digitData = 0x66;
+        break;
+    case 5:
+        digitData = 0x6D;
+        break;
+    case 6:
+        digitData = 0x7D;
+        break;
+    case 7:
+        digitData = 0x27;
+        break;
+    case 8:
+        digitData = 0x7F;
+        break;
+    case 9:
+        digitData = 0x6F;
         break;
     }
 
-    if (!number)
-    {
-        if (dot)
-            send(digitId, 0xBF);
-        else
-            send(digitId, 0x3F);
-    }
-    else
-    {
-        switch (number)
-        {
-        case 1:
-            if (dot)
-                send(digitId, 0x86);
-            else
-                send(digitId, 0x06);
-            break;
-        case 2:
-            if (dot)
-                send(digitId, 0xDB);
-            else
-                send(digitId, 0x5B);
-            break;
-        case 3:
-            if (dot)
-                send(digitId, 0xCF);
-            else
-                send(digitId, 0x4F);
-            break;
-        case 4:
-            if (dot)
-                send(digitId, 0xE6);
-            else
-                send(digitId, 0x66);
-            break;
-        case 5:
-            if (dot)
-                send(digitId, 0xED);
-            else
-                send(digitId, 0x6D);
-            break;
-        case 6:
-            if (dot)
-                send(digitId, 0xFD);
-            else
-                send(digitId, 0x7D);
-            break;
-        case 7:
-            if (dot)
-                send(digitId, 0xA7);
-            else
-                send(digitId, 0x27);
-            break;
-        case 8:
-            if (dot)
-                send(digitId, 0xFF);
-            else
-                send(digitId, 0x7F);
-            break;
-        case 9:
-            if (dot)
-                send(digitId, 0xEF);
-            else
-                send(digitId, 0x6F);
-            break;
-        }
-    }
+    bitWrite(digitData, 7, dot);
+    send(digitId, digitData);
 }
 
 void ch455::showWithDots(uint8_t digit0, bool dot0, uint8_t digit1, bool dot1, uint8_t digit2, bool dot2, uint8_t digit3, bool dot3)
